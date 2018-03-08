a8c44133581fd507ac1166ee3ced8650f7c6ce58
Nihit S. Bhavsar <nihit3092@gmail.com>
2018-03-08 10:37:07 -0600

    Initial Commit

---
+++ flexNN/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  flexNN
+//
+//  Created by Nihit Bhavsar on 3/8/18.
+//  Copyright © 2018 Nihit Bhavsar. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
